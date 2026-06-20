02efe468892d869aa1c5a1130d90b822662aece4
Nabilmln <133762203+Nabilmln@users.noreply.github.com>
2026-06-20 14:30:07 +0700

    feat: implement start and pause timer functionality

--- intermediate-podomoro-timer/pomodoro-timer/src/App.jsx
+++ intermediate-podomoro-timer/pomodoro-timer/src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { useState, useEffect } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
   const [timeLeft, setTimeLeft] = useState(1500);
+  const [isRunning, setIsRunning] = useState(false);
 
   const formatTime = () => {
     const minutes = Math.floor(timeLeft/60);
@@ -13,13 +14,26 @@ function App() {
     return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
   }
 
+  useEffect(() => {
+    if(isRunning){
+      const interval = setInterval(() => {
+        setTimeLeft(prevTime => prevTime - 1);
+      }, 1000);
+
+      return () => clearInterval(interval);
+    }
+  }, [isRunning]);
+
   return (
     <div>
       <h1>Podomoro Timer</h1>
 
       <h2>{formatTime()}</h2>
 
-      <button onClick={() => setTimeLeft(timeLeft - 1)}> Kurangi </button>
+      <button onClick={() => setIsRunning(true)}> Start </button>
+      <button onClick={() => setIsRunning(false)}> Pause </button>
+    
+      
     </div>
   );
 }
