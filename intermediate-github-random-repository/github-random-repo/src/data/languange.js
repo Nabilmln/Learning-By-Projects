fead1c2d41d89ffdb8eeecb670ff491582dd9b97
Nabilmln <133762203+Nabilmln@users.noreply.github.com>
2026-06-21 18:27:07 +0700

    chore: initialize react project

---
+++ intermediate-github-random-repository/github-random-repo/src/data/languange.js
@@ -0,0 +1,14 @@
+export const languages = [
+    "JavaScript",
+    "Python",
+    "Java",
+    "TypeScript",
+    "PHP",
+    "Go",
+    "Rust",
+    "C",
+    "C++",
+    "C#",
+    "Kotlin",
+    "Swift",
+];
