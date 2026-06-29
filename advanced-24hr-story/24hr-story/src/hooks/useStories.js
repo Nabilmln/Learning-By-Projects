2c4061a2f4c6e168445891cd5239f21020cc42ee
Nabilmln <133762203+Nabilmln@users.noreply.github.com>
2026-06-29 21:52:37 +0700

    refactor: organize story logic into reusable custom hook

--- advanced-24hr-story/24hr-story/src/hooks/useStories.js
+++ advanced-24hr-story/24hr-story/src/hooks/useStories.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import { resizeImage } from "./utils/imageResize";
-import { loadStories, saveStories } from "./utils/storyStorage";
+import { resizeImage } from "../utils/imageResize";
+import { loadStories, saveStories } from "../utils/storyStorage";
 
 
 export function useStories(){
@@ -9,7 +9,7 @@ export function useStories(){
     const [progress, setProgress] = useState(0);
     const ONE_DAY = 24 * 60 * 60 * 1000;
     const [uploading, setUploading] = useState(false);
-    
+
     const handleUpload = async (e) => {
     const file = e.target.files[0];
 
@@ -27,7 +27,11 @@ export function useStories(){
         viewed: false,
       };
 
-      const updatedStories = [...stories, newStory];
+      setStories((prevStories) => {
+        const updatedStories = [...prevStories, newStory];
+        saveStories(updatedStories);
+        return updatedStories;
+      });
 
       setStories(updatedStories);
 
@@ -76,7 +80,7 @@ export function useStories(){
     const updatedStories = [...stories];
     updatedStories[index].viewed = true;
     setStories(updatedStories);
-    localStorage.setItem("stories", JSON.stringify(updatedStories));
+    // localStorage.setItem("stories", JSON.stringify(updatedStories));
   };
 
   useEffect(() => {
@@ -85,7 +89,7 @@ export function useStories(){
       return Date.now() - story.createdAt < ONE_DAY;
     });
     setStories(validStories);
-    localStorage.setItem("stories", JSON.stringify(validStories));
+    // localStorage.setItem("stories", JSON.stringify(validStories));
   }, []);
 
   useEffect(() => {
@@ -117,7 +121,8 @@ export function useStories(){
         const validStories = prevStories.filter((story) => {
           return Date.now() - story.createdAt < ONE_DAY;
         });
-        localStorage.setItem("stories", JSON.stringify(validStories));
+        setStories(validStories);
+        // localStorage.setItem("stories", JSON.stringify(validStories));
         return validStories;
       });
     }, 60000);
